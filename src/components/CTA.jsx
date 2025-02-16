af9ff550f110d86beefeb9c47244521cff6c0087
Andrew Nguyen <anguyen0615@gmail.com>
2025-02-15 23:29:39 -0600

    style: Update Hero button background and restructure CTA component
    - Modify Hero component with new button background color scheme
    - Restructure CTA component layout and styling

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,21 +1,19 @@
 const CTA = () => {
     return (
-        <div className="bg-[#e4b1b1]">
-            <div className="flex items-center flex-col mx-auto max-w-7xl px-6 py-24 lg:px-8">
-                <h2 className="text-center max-w-2xl text-4xl font-semibold tracking-tight text-balance text-white sm:text-5xl">
-                    Make Your Event Unforgettable!
+        <div className="bg-indigo-100">
+            <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:justify-between lg:px-8">
+                <h2 className="max-w-2xl text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                    Ready to dive in? <br />
+                    Start your free trial today.
                 </h2>
-                <p className="mt-6 text-center text-lg/8 max-w-2xl font-semibold tracking-tight text-balance text-white">
-                    At The Bubble House Rentals, we believe every celebration deserves a touch of elegance and a whole lot of fun.
-                </p>
-                <div className="mt-10 flex items-center gap-x-6">
+                <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:shrink-0">
                     <a
                         href="#"
-                        className="rounded-md bg-[#a38f8b] px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                         Get started
                     </a>
-                    <a href="#" className="text-sm/6 font-semibold text-white">
+                    <a href="#" className="text-sm/6 font-semibold text-gray-900">
                         Learn more <span aria-hidden="true">→</span>
                     </a>
                 </div>
